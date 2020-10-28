2304cd27a0f2ad82c663439dd34078fb526def03
Geno Ferollo <genemators@gmail.com>
2020-10-28 06:18:02 +0500

    formatting whole project

--- action/middlewares/links.js
+++ action/middlewares/links.js
@@ -1,22 +1,22 @@
 const { composer, middleware } = require("../../core/bot");
 
-const gifs = require("../../database/db").gifs
+const gifs = require("../../database/db").gifs;
 const consoles = require("../../layouts/consoles");
 const message = require("../../layouts/messages");
 const keyboard = require("../../layouts/keyboards");
 const database = require("../../database/db");
 
 composer.command(`links`, async (ctx) => {
-  const links = database.links["links"];
+	const links = database.links["links"];
 
-  await ctx.replyWithAnimation(
-    { url: gifs.links },
-    {
-      caption: message.links,
-      parse_mode: "HTML",
-      reply_markup: await keyboard.links(links),
-    }
-  );
+	await ctx.replyWithAnimation(
+		{ url: gifs.links },
+		{
+			caption: message.links,
+			parse_mode: "HTML",
+			reply_markup: await keyboard.links(links),
+		}
+	);
 });
 
 middleware(composer);
--- action/middlewares/start.js
+++ action/middlewares/start.js
@@ -1,19 +1,19 @@
 const { composer, middleware } = require("../../core/bot");
 
-const gifs = require("../../database/db").gifs
+const gifs = require("../../database/db").gifs;
 const consoles = require("../../layouts/consoles");
 const message = require("../../layouts/messages");
 const keyboard = require("../../layouts/keyboards");
 
 composer.start(async (ctx) => {
-  await ctx.replyWithAnimation(
-    { url: gifs.start },
-    {
-      parse_mode: "HTML",
-      caption: message.start,
-      reply_markup: keyboard.start,
-    }
-  );
+	await ctx.replyWithAnimation(
+		{ url: gifs.start },
+		{
+			parse_mode: "HTML",
+			caption: message.start,
+			reply_markup: keyboard.start,
+		}
+	);
 });
 
 middleware(composer);
