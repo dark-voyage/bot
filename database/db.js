a5e13336a1ca8106c4673fa10701558077648f5d
Geno Ferollo <genemators@gmail.com>
2020-10-28 06:06:32 +0500

    removing un files

--- database/db.js
+++ database/db.js
@@ -1,5 +1,4 @@
 exports.admins = require("./json/admins.json");
 exports.users = require("./json/users.json");
-exports.teams = require("./json/teams.json");
 exports.links = require("./json/links.json");
 exports.gifs = require("./json/gifs.json");
