5d4a98481317ce31bf3031eed85337a222c69617
Geno Ferollo <genemators@gmail.com>
2020-10-28 06:22:45 +0500

    Update index.js

--- action/inline/index.js
+++ action/inline/index.js
@@ -8,64 +8,40 @@ const keyboard = require("../../layouts/keyboards");
 const ds = require("../../database/ds");
 
 composer.on("inline_query", async ({ inlineQuery, answerInlineQuery }) => {
-	let results = [],
-		indexation = 1,
-		base = `https://github.com/genemators/`,
-		thumb = `https://genemator.me/favicon.png`;
-	let repos = [];
-	await repos.push(
-		Object.values(
-			await ds("https://api.github.com/users/genemators/repos")
-		).map(function (obj) {
-			return obj["name"];
-		})
-	);
-	await repos.push(
-		Object.values(
-			await ds("https://api.github.com/orgs/00010023/repos")
-		).map(function (obj) {
-			return obj["name"];
-		})
-	);
-	await repos.push(
-		Object.values(
-			await ds("https://api.github.com/orgs/chiseldevs/repos")
-		).map(function (obj) {
-			return obj["name"];
-		})
-	);
-	await repos.push(
-		Object.values(await ds("https://api.github.com/orgs/4bis1/repos")).map(
-			function (obj) {
-				return obj["name"];
-			}
-		)
-	);
-	let similarities = await fuzzy
-		.filter(inlineQuery.query, repos)
-		.sort()
-		.slice(0, 20);
-	let found = await similarities.map(function (obj) {
-		return obj.string;
-	});
-	for (let key of found) {
-		let data = await ds(`https://api.github.com/repos/genemators/${key}`);
-		results.push({
-			type: "article",
-			id: indexation,
-			url: base + key,
-			title: key,
-			description: `${data["description"]}`,
-			reply_markup: keyboard.inline(data),
-			input_message_content: {
-				message_text: message.inline(data),
-				parse_mode: "HTML",
-				disable_web_page_preview: true,
-			},
-		});
-		indexation++;
-	}
-	return answerInlineQuery(results);
+  let results = [],
+    indexation = 1,
+    base = `https://github.com/genemators/`,
+    thumb = `https://genemator.me/favicon.png`;
+  let repos = await Object.values(
+    await ds("https://api.github.com/users/genemators/repos")
+  ).map(function (obj) {
+    return obj["name"];
+  });
+  let similarities = await fuzzy
+    .filter(inlineQuery.query, repos)
+    .sort()
+    .slice(0, 20);
+  let found = await similarities.map(function (obj) {
+    return obj.string;
+  });
+  for (let key of found) {
+    let data = await ds(`https://api.github.com/repos/genemators/${key}`);
+    results.push({
+      type: "article",
+      id: indexation,
+      url: base + key,
+      title: key,
+      description: `${data["description"]}`,
+      reply_markup: keyboard.inline(data),
+      input_message_content: {
+        message_text: message.inline(data),
+        parse_mode: "HTML",
+        disable_web_page_preview: true,
+      },
+    });
+    indexation++;
+  }
+  return answerInlineQuery(results);
 });
 
 middleware(composer);
